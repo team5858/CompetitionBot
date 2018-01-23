69308f645b54cbdfa9e6e45d0810008518cfe349
goldenhurricanes <goldenhurricanes@10.194.147.106>
2018-01-23 16:52:14 -0600

    drivetrain stuff...

--- CompetitionBot/src/org/usfirst/frc5858/CompetitionBot/subsystems/Drivetrain.java
+++ CompetitionBot/src/org/usfirst/frc5858/CompetitionBot/subsystems/Drivetrain.java
@@ -34,9 +34,19 @@ import edu.wpi.first.wpilibj.Talon;
 public class Drivetrain extends Subsystem {
 
 	public final TalonSRX left_Speed_Controller = RobotMap.drivetrainLeft_Speed_Controller;
-    private final SpeedController right_Speed_Controller = RobotMap.drivetrainRight_Speed_Controller;
+    private final TalonSRX right_Speed_Controller = RobotMap.drivetrainRight_Speed_Controller;
     //private final DifferentialDrive robot_Drive_rd = new DifferentialDrive((SpeedController) left_Speed_Controller, right_Speed_Controller);
-
+   
+    public void setSpeed (double right, double left) {
+		left_Speed_Controller.set(ControlMode.PercentOutput, left);
+		right_Speed_Controller.set(ControlMode.PercentOutput, right);
+    }
+    public void gofoward (double distanceFeet) {
+    	
+    }
+    public void spin (double angleDegrees) {
+    	
+    }
     @Override
     public void initDefaultCommand() {
     	setDefaultCommand(new ZTEST_RunTalonJoystick());
