482aebd21ebb68dd97be7fefd8314411fd42f736
tophe <tophe@10.194.160.141>
2018-02-03 13:55:01 -0600

    temporary code

--- CompetitionBot/src/org/usfirst/frc5858/CompetitionBot/commands/Drive_Motors.java
+++ CompetitionBot/src/org/usfirst/frc5858/CompetitionBot/commands/Drive_Motors.java
@@ -21,7 +21,7 @@ public class Drive_Motors extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.drivetrain.setSpeed(right, left);
+    	//Robot.drivetrain.setSpeed(right, left);
     }
 
     // Called repeatedly when this Command is scheduled to run
